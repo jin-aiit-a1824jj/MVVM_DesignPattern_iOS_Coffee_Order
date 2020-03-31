999c174b123056194ec19da298dbd2914f3a7afc
a1824jj <a1824jj@aiit.ac.jp>
2020-03-31 10:46:17 +0900

    add viewmodel

---
+++ HotCoffee-sampel/ViewModel/OrderViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  OrderViewModel.swift
+//  HotCoffee-sampel
+//
+//  Created by JONGWOO JIN on 2020/03/31.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import Foundation
+
+class OrderListViewModel {
+    var ordersViewModel: [OrderViewModel]
+    
+    init() {
+        self.ordersViewModel = [OrderViewModel]()
+    }
+}
+
+extension OrderListViewModel {
+    func orderViewModel(at index: Int) -> OrderViewModel {
+        return self.ordersViewModel[index]
+    }
+}
+
+
+struct OrderViewModel {
+    let order: Order
+}
+
+extension OrderViewModel {
+    var name: String {
+        return self.order.name
+    }
+    
+    var email: String {
+        return self.order.email
+    }
+    
+    var type: String {
+        return self.order.type.rawValue.capitalized
+    }
+    
+    var size: String {
+        return self.order.size.rawValue.capitalized
+    }
+    
+}
