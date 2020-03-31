af8b6b1d3e1897ff4ca6494b1ccfe5b8045bf984
a1824jj <a1824jj@aiit.ac.jp>
2020-03-31 11:37:35 +0900

    populating model using viewmodel

--- HotCoffee-sampel/Models/Order.swift
+++ HotCoffee-sampel/Models/Order.swift
@@ -27,3 +27,19 @@ struct  Order: Codable {
     let type: CoffeeType
     let size: CoffeSize
 }
+
+extension Order {
+    init?(_ vm: AddCoffeeOrderViewModel){
+        guard let name = vm.name,
+                 let email = vm.email,
+                 let selectecType = CoffeeType(rawValue: vm.selectedType!.lowercased()),
+                 let selectedSize = CoffeSize(rawValue: vm.selectedSize!.lowercased()) else {
+                return nil
+        }
+        
+        self.name = name
+        self.email = email
+        self.type = selectecType
+        self.size = selectedSize
+    }
+}
